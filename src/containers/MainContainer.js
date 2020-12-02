dbfacdd3c4b1aaeee36ca6390222288417128ea9
Craig McLeod <mcleod.craig@outlook.com>
2020-12-02 11:57:36 +0000

    Axios installed, launch state and data fetch functions implemented

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,8 +2,21 @@ import React, { useState, useEffect } from 'react'
 import FilterContainer from './FilterContainer'
 import HeaderContainer from './HeaderContainer'
 import LaunchListContainer from './LaunchListContainer'
+import axios from 'axios'
 
 const MainContainer = () => {
+
+    const [ spaceXLaunches, setSpaceXLaunches ] = useState([ ])
+    const [ apiURL, setApiURL ] = useState('https://api.spacexdata.com/v3/launches')
+
+    const fetchData = async ( url ) => {
+        const spaceXAPIResults = await axios( url )
+        setSpaceXLaunches( spaceXAPIResults.data )
+    }
+
+    useEffect(() => {
+        fetchData(apiURL)
+    })
 
     return(
         <>
